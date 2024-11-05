257b71449cadd40dd479247ac5814c10977f2f55
Baba Gandhi <babagandhi>
2024-11-06 00:44:45 +0530

    Testing1

--- Node.js
+++ Node.js
@@ -9,6 +9,7 @@ const server = http.createServer((request, response) => {
         response.writeHead(200, { 'Content-Type': 'text/html' });
         response.end('<h1>Hello KMIT!</h1>');
     }
+    // adding to priyaaaa
     else if (path === '/products') {
         let productRows = products.map(product => `
             <tr>
